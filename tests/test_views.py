b407bf5eea746a3896175106d6b7b6c0dde72496
henry <tahafut@gmail.com>
2019-11-26 09:09:45 -0800

    add fixture

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,16 +1,39 @@
 import pytest
 from src.views import app
+from src import views
 
 
 @pytest.fixture
 def client():
+    views.students = {
+        "Alice": {
+            "results": [{"exam": 3, "score": 0.9}, {"exam": 4, "score": 0.7}],
+            "average": (0.7 + 0.9) / 2,
+        },
+        "Bob": {"results": [{"exam": 3, "score": 0.8}], "average": 0.8},
+    }
+
+    views.exams = {
+        3: {
+            "results": [
+                {"studentId": "Alice", "score": 0.9},
+                {"studentId": "Bob", "score": 0.8},
+            ],
+            "average": (0.8 + 0.9) / 2,
+        },
+        4: {"results": [{"studentId": "Alice", "score": 0.7}], "average": 0.7},
+    }
+
     c = app.test_client()
     yield c
+
+    views.students = {}
+    views.exams = {}
 
 
 def test_students(client):
     r = client.get("/students", follow_redirects=True)
-    assert r.get_json() == []
+    assert r.get_json() == ["Alice", "Bob"]
 
 
 def test_student_not_found(client):
@@ -21,10 +44,10 @@ def test_student_not_found(client):
 
 def test_exams(client):
     r = client.get("/exams", follow_redirects=True)
-    assert r.get_json() == []
+    assert r.get_json() == [3, 4]
 
 
 def test_exam_not_found(client):
-    r = client.get("/exams/3", follow_redirects=True)
+    r = client.get("/exams/2", follow_redirects=True)
     assert r.status_code == 404
     assert r.get_json() == {"error": "404 Not Found: exam does not exist"}

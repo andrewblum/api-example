507234974c890ea723dc1f685c3b828c52ba3b6f
henry <tahafut@gmail.com>
2019-11-26 09:13:15 -0800

    add tests for student and exam details

--- tests/test_views.py
+++ tests/test_views.py
@@ -36,6 +36,14 @@ def test_students(client):
     assert r.get_json() == ["Alice", "Bob"]
 
 
+def test_student_detail(client):
+    r = client.get("/students/Alice")
+    assert r.get_json() == {
+        "results": [{"exam": 3, "score": 0.9}, {"exam": 4, "score": 0.7}],
+        "average": (0.7 + 0.9) / 2,
+    }
+
+
 def test_student_not_found(client):
     r = client.get("/students/not_such_student", follow_redirects=True)
     assert r.status_code == 404
@@ -45,6 +53,17 @@ def test_student_not_found(client):
 def test_exams(client):
     r = client.get("/exams", follow_redirects=True)
     assert r.get_json() == [3, 4]
+
+
+def test_exam_detail(client):
+    r = client.get("/exams/3")
+    assert r.get_json() == {
+        "results": [
+            {"studentId": "Alice", "score": 0.9},
+            {"studentId": "Bob", "score": 0.8},
+        ],
+        "average": (0.8 + 0.9) / 2,
+    }
 
 
 def test_exam_not_found(client):
